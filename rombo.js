fbf2326fabf007c2d81bad6d6249409b811de1ab
jtorresarias <111446114+jtorresarias@users.noreply.github.com>
2023-02-22 10:25:55 -0600

    ejercicio rombo

---
+++ rombo.js
@@ -0,0 +1,12 @@
+var rombo =["    *    ", "   ***   ","  *****  "," ******* ","*********"]
+ 
+for(i = 0; i < rombo.length; i++){
+    console.log(rombo[i]);
+}
+for(i = 4; i > 0; i--){
+    console.log(rombo[i]);
+}
+
+
+
+ 
