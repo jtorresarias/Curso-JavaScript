2827f9098c72fe11b33c196399c19c7bab31bb0d
jtorresarias <111446114+jtorresarias@users.noreply.github.com>
2023-02-22 10:25:15 -0600

    ejercicio matriz 4x4

---
+++ matrix4x4.js
@@ -0,0 +1,12 @@
+var caracter = "x";
+var fila = " ";
+var columna = " ";
+for(i = 0; i < 4; i++){
+    fila = caracter;
+    for(j = 1; j < 4; j++){
+    columna += caracter
+    }
+    console.log(fila+columna);
+    columna =" "
+}
+
